c6aefe426763f8f14b80e961f1d7978c867d9928
surovikindmitry <surovikindmitry@gmail.com>
2024-11-28 21:46:29 +0300

    2

---
+++ app/admin_request.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from config.bot_config import ADMIN_ID
+
+
+storage = MemoryStorage
+
+async def send_greting()
+
--- config/bot_config.py
+++ config/bot_config.py
@@ -3,4 +3,5 @@ from dotenv import dotenv_values
 
 config = dotenv_values('./config/.env')
 API_TOKEN = config['TOKEN']
+ADMIN_ID = config['ADMIN']
 
